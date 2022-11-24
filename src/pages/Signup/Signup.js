81aafcb0f1690ebea4823ba29c86f56f29fc7d29
Amena Akhter Hira <amena35-2139@diu.edu.bd>
2022-11-24 00:08:46 -0800

    sign up implement

--- src/pages/Signup/Signup.js
+++ src/pages/Signup/Signup.js
@@ -1,38 +1,55 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../../context/AuthProvider';
 
 const Signup = () => {
+    const { createUser } = useContext(AuthContext);
+    const handleCreateUser = (event) =>{
+        event.preventDefault();
+        const form = event.target;
+        const name = form.name.value;
+        const photoURL = form.photoURL.value;
+        const email = form.email.value;
+        const password = form.password.value;
+        console.log(name,photoURL,email, password);
+        createUser(email,password)
+        .then((userCredential) => {
+            const user = userCredential.user;
+            console.log(user);
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+
+    }
     return (
         <div className='flex justify-center pt-12 '>
             <div className="card flex-shrink-0 w-full max-w-md shadow-2xl bg-base-100 drop-shadow-2xl">
-                <form className="card-body">
+                <form onSubmit={handleCreateUser} className="card-body">
                     <h2 className='text-center text-4xl'>Sign Up</h2>
                     <div className="form-control">
                         <label className="label">
                             <span className="label-text">Name</span>
                         </label>
-                        <input type="text" placeholder="Name" className="input input-bordered" />
+                        <input type="text" name='name' placeholder="Name" className="input input-bordered" />
                     </div>
                     <div className="form-control">
                         <label className="label">
                             <span className="label-text">PhotoURL</span>
                         </label>
-                        <input type="text" placeholder="Photo URL" className="input input-bordered" />
+                        <input type="text" name='photoURL' placeholder="Photo URL" className="input input-bordered" />
                     </div>
                     <div className="form-control">
                         <label className="label">
                             <span className="label-text">Email</span>
                         </label>
-                        <input type="email" placeholder="Email" className="input input-bordered" />
+                        <input type="email" name='email' placeholder="Email" className="input input-bordered" />
                     </div>
                     <div className="form-control">
                         <label className="label">
                             <span className="label-text">Password</span>
                         </label>
-                        <input type="password" placeholder="Password" className="input input-bordered" />
-                        <label className="label">
-                            <Link href="#" className="label-text-alt link link-hover">Forgot password?</Link>
-                        </label>
+                        <input type="password" name='password' placeholder="Password" className="input input-bordered" />
                     </div>
                     <div className="form-control my-2">
                         <button className="btn border-none bg-sky-500 hover:bg-sky-900">Signup</button>
