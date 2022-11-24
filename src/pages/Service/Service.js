62098de3dde3434aa109842c2f70e6f6d2d5b756
Amena Akhter Hira <amena35-2139@diu.edu.bd>
2022-11-24 01:26:06 -0800

    service added in mongodb

--- src/pages/Service/Service.js
+++ src/pages/Service/Service.js
@@ -2,34 +2,58 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Service = () => {
+    const handleSubmitService = (event) =>{
+        event.preventDefault();
+        const form = event.target;
+        const name = form.name.value;
+        const photoURL = form.photoURL.value;
+        const price = form.price.value;
+        const details = form.details.value;
+        console.log(name, photoURL, price, details);
+        const service = {
+            serviceName: name,
+            image: photoURL,
+            price,
+            details
+        }
+        fetch('http://localhost:5000/services',{
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json',
+            },
+            body: JSON.stringify(service)
+        })
+        .then(res => res.json())
+        .then(data => console.log(data))
+    }
     return (
         <div className='flex justify-center pt-8 '>
             <div className="card flex-shrink-0 w-full max-w-md shadow-2xl bg-base-100 drop-shadow-2xl">
-                <form className="card-body">
+                <form onSubmit={handleSubmitService} className="card-body">
                     <h2 className='text-center text-4xl'>Add A Service</h2>
                     <div className="form-control">
                         <label className="label">
                             <span className="label-text">Service Name</span>
                         </label>
-                        <input type="text" placeholder="Name" className="input input-bordered" />
+                        <input type="text" name='name' placeholder="Name" className="input input-bordered" />
                     </div>
                     <div className="form-control">
                         <label className="label">
                             <span className="label-text">PhotoURL</span>
                         </label>
-                        <input type="text" placeholder="Photo URL" className="input input-bordered" />
+                        <input type="text" name='photoURL' placeholder="Photo URL" className="input input-bordered" />
                     </div>
                     <div className="form-control">
                         <label className="label">
                             <span className="label-text">Price</span>
                         </label>
-                        <input type="text" placeholder="Photo URL" className="input input-bordered" />
+                        <input type="text" name='price' placeholder="Price" className="input input-bordered" />
                     </div>
                     <div className="form-control">
                         <label className="label">
                             <span className="label-text">Details</span>
                         </label>
-                        <textarea className="textarea textarea-bordered" placeholder="Bio"></textarea>
+                        <textarea name='details' className="textarea textarea-bordered" placeholder="Service Details"></textarea>
                     </div>
                     <div className="form-control my-2">
                         <button className="btn border-none bg-sky-500 hover:bg-sky-900">Submit</button>
