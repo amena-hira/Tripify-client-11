6f0e9d2879be1f54c2a48e47c39ba9abbed042bc
Amena Akhter Hira <amena35-2139@diu.edu.bd>
2022-11-24 01:30:48 -0800

    logout implemented

--- src/context/AuthProvider.js
+++ src/context/AuthProvider.js
@@ -1,5 +1,5 @@
 import React, { createContext, useEffect, useState } from 'react';
-import {createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword} from 'firebase/auth';
+import {createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut} from 'firebase/auth';
 import app from '../firebase/firebase.config';
 
 export const AuthContext = createContext();
@@ -15,6 +15,10 @@ const AuthProvider = ({children}) => {
     const login = (email, password) =>{
         setLoading(true);
         return signInWithEmailAndPassword(auth, email, password);
+    }
+    const logOut = () =>{
+        localStorage.removeItem('genius-token');
+        return signOut(auth);
     }
 
     useEffect( () =>{
@@ -33,7 +37,8 @@ const AuthProvider = ({children}) => {
         user, 
         loading,
         createUser, 
-        login
+        login,
+        logOut
     } 
     return (
         <AuthContext.Provider value={authInfo}>
--- src/pages/Shared/Header.js
+++ src/pages/Shared/Header.js
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../../context/AuthProvider';
 import TopHeadSocial from './TopHeadSocial';
 
 const Header = () => {
+    const {user, logOut} = useContext(AuthContext);
+    const handleLogOut = () =>{
+        logOut()
+        .then(()=>{})
+        .catch(error => console.log(error))
+    }
     return (
         <div className='bg-cyan-600 text-white'>
             <div className='max-w-screen-xl py-3 mx-auto'>
@@ -15,7 +22,13 @@ const Header = () => {
                         <ul className="menu menu-horizontal p-0">
                             <li><Link to='/addservice'>Add Service</Link></li>
                             <li><Link>My Reviews</Link></li>
-                            <li><Link to='/login'>Login</Link></li>
+                            {
+                                user? 
+                                <li><Link onClick={handleLogOut}>logout</Link></li>
+                                :
+                                <li><Link to='/login'>Login</Link></li>
+                            }
+                            
                         </ul>
                     </div>
                 </div>
