84e79f0f8bb0e1f8516a653255c5b864c9f733ea
Amena Akhter Hira <amena35-2139@diu.edu.bd>
2022-11-26 13:13:42 -0800

    title and spinner issue fixed

--- src/Router/PrivateRoute/PrivateRoute.js
+++ src/Router/PrivateRoute/PrivateRoute.js
@@ -1,13 +1,17 @@
 import React, { useContext } from 'react';
 import { Navigate, useLocation } from 'react-router-dom';
 import { AuthContext } from '../../context/AuthProvider';
+import spinner from '../../assets/loading.gif';
 
 const PrivateRoute = ({children}) => {
     const {user, loading} = useContext(AuthContext);
     const location  = useLocation();
+    console.log(loading);
 
     if(loading) {
-        return <svg className="animate-bounce w-6 h-6 ..."></svg>
+        return <div>
+            <img src={spinner} style={{height: '80px'}} alt="spinner" />
+        </div>
     }
 
     if(user){
